bfbc3de45452899b13f708caf78a6a93ed4c204a
Jose Carlos Estrada Garcia <201212644@ingenieria.usac.edu.gt>
2018-11-08 19:12:22 -0600

    Declaraciones de tipos

---
+++ clase2.js
@@ -0,0 +1,26 @@
+let num1=1;
+let num2=2;
+
+let res= num1 + num2;
+
+
+
+var prueba=1;
+
+
+//CallBack de ForEach
+process.argv.forEach((value,index)=> {
+    console.log(`Indice : ${index}, value : ${value} `);
+})
+
+
+
+
+let funcion = (param1,param2) =>{
+return param1+param2;
+
+}
+
+console.log(`La funcion da como resultado:  ${funcion(10,5)}`);
+
+
